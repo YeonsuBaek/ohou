e926178741ce1fa354ae2e31f1bd9da4dcb780d4
YeonsuBaek <soothbugger@gmail.com>
2023-05-28 20:42:51 +0900

    style: Sidebar 로그아웃 버튼 추가

--- src/Layouts/Sidebar.js
+++ src/Layouts/Sidebar.js
@@ -3,21 +3,52 @@ import styles from './Sidebar.module.css';
 import { ReactComponent as Logo } from '../assets/icons/logo.svg';
 import PrimaryButton from '../components/UI/Buttons/PrimaryButton';
 import { Link } from 'react-router-dom';
+import useUserStore from '../stores/useUserStore';
+import useAdminStore from '../stores/useAdminStore';
+import { auth } from '../firebase-config';
 
 const Sidebar = () => {
+  const { userEmail, userName, setUserEmail, setUserName } = useUserStore();
+  const { adminID, setAdminID, setAdminPW } = useAdminStore();
+
+  const handleLogout = () => {
+    if (adminID) {
+      setAdminID('');
+      setAdminPW('');
+    }
+
+    if (userEmail) {
+      auth
+        .signOut()
+        .then(() => {
+          setUserEmail('');
+          setUserName('');
+        })
+        .catch((error) => {
+          console.error('로그아웃 중 에러 발생:', error);
+        });
+    }
+  };
+
   return (
     <aside className={styles.sidebar}>
       <header className={styles.header}>
         <Link to='/' className={styles.logo} aria-label='오늘의집'>
           <Logo />
         </Link>
-        <Link to='/login' className={styles.login} aria-label='로그인'>
-          <PrimaryButton text='로그인' />
-        </Link>
-
-        {/* NOTE: 회원 로그인 */}
-        {/* <strong className={styles.name}>홍길동님</strong> */}
+        {userEmail || adminID ? (
+          <strong className={styles.name}>{userName || adminID}님</strong>
+        ) : (
+          <Link to='/login' className={styles.login} aria-label='로그인'>
+            <PrimaryButton text='로그인' />
+          </Link>
+        )}
       </header>
+      {(userEmail || adminID) && (
+        <button className={styles.logout} type='button' onClick={handleLogout}>
+          로그아웃
+        </button>
+      )}
     </aside>
   );
 };
